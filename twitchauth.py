b1ae8aa176093ab7362cecfca77fea059afc00fd
WillyJimmyDev <ben@williamjames.dev>
2022-01-08 18:59:32 +0000

    Used .env file for credentials

--- twitchauth.py
+++ twitchauth.py
@@ -1,7 +1,8 @@
 from PySide2.QtWebEngineWidgets import QWebEngineView
 from PySide2.QtWebEngineCore import QWebEngineUrlRequestInterceptor
 from PySide2.QtCore import QUrl
-
+import os
+from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
@@ -21,11 +22,14 @@ class WebEngineUrlRequestInterceptor(QWebEngineUrlRequestInterceptor):
 
 class TwitchAuthRequest:
 
+    load_dotenv()
+    twitch_client_id = os.getenv('T_CLIENT_ID')
+
     def __init__(self):
         self.browser = QWebEngineView()
 
     def authenticate(self):
-        self.browser.load(QUrl("https://id.twitch.tv/oauth2/authorize?client_id=4dcpxwjtz5n2h0323mrutraf8k8gkc"
+        self.browser.load(QUrl("https://id.twitch.tv/oauth2/authorize?client_id=" + self.twitch_client_id +
                                "&redirect_uri=http://localhost&response_type=token+id_token&scope=openid "
                                "user:read:follows"))
         self.browser.show()
