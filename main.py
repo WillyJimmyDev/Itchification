545f76c34ea7d3f8a9f9d37b5d7c6ccaa41e4756
WillyJimmyDev <ben@williamjames.dev>
2022-01-12 22:46:42 +0000

    Trying and failing to load icon from url with QNetworkAccessManager()

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import os
-from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QListView, QMessageBox
+
+from PySide2 import QtCore, QtGui
+from PySide2.QtCore import QUrl
+from PySide2.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PySide2.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QListView, QMessageBox
 from db.database import ItchificationDB
-from PySide2.QtGui import QStandardItemModel, QStandardItem
+from PySide2.QtGui import QStandardItemModel, QStandardItem, QIcon, QPixmap
 from twitch import Twitch, WebEngineUrlRequestInterceptor
 from PySide2.QtWebEngineWidgets import QWebEngineProfile
 
@@ -14,6 +18,9 @@ if __name__ == '__main__':
     class MainWindow(QMainWindow):
 
         def __init__(self):
+            self.nam = QNetworkAccessManager()
+            self.item_icon = QIcon()
+
             self.twitch = Twitch()
             self.dbconn = ItchificationDB()
             super().__init__()
@@ -25,7 +32,15 @@ if __name__ == '__main__':
             model = QStandardItemModel(self.list_widget)
             followed_list = self.twitch.get_followed_list()
             for f in followed_list:
-                item = QStandardItem(f["display_name"])
+                display_name = f["display_name"]
+                icon = QtCore.QUrl(f["profile_image_url"])
+
+                item = QStandardItem(display_name)
+                self.nam.finished.connect(self.set_item_icon)
+                self.nam.get(QNetworkRequest(icon))
+                item.setData("https://twitch.tv/" + display_name, 257)  # 257 refers to a custom user role enum
+                print(self.item_icon)  # null
+                item.setIcon(self.item_icon)
                 model.appendRow(item)
 
             self.list_widget.clicked.connect(self.on_item_changed)
@@ -37,10 +52,18 @@ if __name__ == '__main__':
             self.setCentralWidget(widget)
             self.setMinimumSize(300, 800)
 
-        def on_item_changed(self, index):
-            item = self.list_widget.selectedIndexes()[0]
-            print(item.model().itemFromIndex(index).text())
+        @staticmethod
+        def on_item_changed(index):
+            print(index.model().itemFromIndex(index).text())
+            url = index.model().itemFromIndex(index).data()
+            channel_link = QtCore.QUrl(url)
+            if not QtGui.QDesktopServices.openUrl(channel_link):
+                QtGui.QMessageBox.warning(None, 'Open Url', 'Could not open url')
 
+        def set_item_icon(self, http_response):
+            pixmap = QPixmap()
+            pixmap.loadFromData(http_response.readAll())
+            self.item_icon = QIcon(pixmap)
 
 
     window = MainWindow()
